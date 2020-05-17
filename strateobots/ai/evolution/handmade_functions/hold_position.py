07b4f7f9cf017150b37a204e108d1777b8c54313
Yaroslav Protsenko <yaroslav_protsenko@outlook.com>
2020-05-17 16:31:37 +0300

    WIP on evolutionary approach: add hold-position algorithm based on evocore

--- strateobots/ai/evolution/handmade_functions/hold_position.py
+++ strateobots/ai/evolution/handmade_functions/hold_position.py
@@ -9,10 +9,11 @@ from .lib import (
 )
 
 
-class LongDistanceAttack(BaseAlgorithm):
+class HoldPosition(BaseAlgorithm):
     def _duel_control(self, bot, enemy):
 
         ctl = keep_distance(bot, enemy)
+        ctl["move"] = self.graph.const(0)
         tower_rotate, _ = navigate_gun(bot, enemy)
         ctl["tower_rotate"] = tower_rotate
 
@@ -29,16 +30,12 @@ class LongDistanceAttack(BaseAlgorithm):
         has_some_shield = bot["shield"] > 0.1
         bot_should_fire = should_fire(bot, enemy, bot_shot_range)
         act_shield_warmup = (1 - bot_should_fire) * has_some_shield * should_retreat
-        act_acceleration = (
-            (1 - bot_should_fire) * (1 - has_some_shield) * should_retreat
-        )
         act_fire = bot_should_fire
-        act_idle = (1 - act_fire) * (1 - act_shield_warmup) * (1-act_acceleration)
+        act_idle = (1 - act_fire) * (1 - act_shield_warmup)
 
         ctl.update(
             {
                 Action.FIRE: act_fire,
-                Action.ACCELERATION: act_acceleration,
                 Action.SHIELD_WARMUP: act_shield_warmup,
                 Action.IDLE: act_idle,
             }
